c24111b087f6c98b796484f9ec941ad4f9e877b2
MatteoLoPiccolo <lostsoul81x@gmail.com>
2024-05-15 12:52:56 +0200

    Creation of Tank

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+}
